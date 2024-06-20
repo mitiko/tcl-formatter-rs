4c8db00a85b28a56f67b5a8d61659904ec24809f
Dimitar Rusev <drusev@efellows.bg>
2024-06-20 10:28:19 +0300

    try format all the TCL files

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,31 @@ use lexer::*;
 use parser::*;
 
 fn main() -> io::Result<()> {
-    let path = "../a1-gtp-proxy/src/GTP-C-clientAcceptV5.tcl";
-    let buf = std::fs::read(path)?;
-    let tokens = Lexer::new().lex(buf).expect("Failed to lex");
-    // print all tokens
-    // for token in tokens.iter() {
-    //     println!("{:?}", token);
-    // }
-    // print comments only
-    // for (t1, t2) in tokens.iter().zip(tokens.iter().skip(1)) {
-    //     if let Token::Other(_) = t2 {
-    //         println!("{:?}{:?}", t1, t2);
-    //     }
-    // }
-    // debug bracket balance
-    // let cnt_lbracket = tokens.iter().filter(|&x| matches!(x, Token::LCurlyBracket)).count();
-    // let cnt_rbracket = tokens.iter().filter(|&x| matches!(x, Token::RCurlyBracket)).count();
-    // dbg!(cnt_lbracket);
-    // dbg!(cnt_rbracket);
-    let ast = Parser::new().parse(&tokens).expect("Failed to parse");
-    let buf = Formatter::new().format(ast); // cursed interface
-    std::fs::write(path, buf)?;
+    for path in [
+        "../a1-gtp-proxy/src/GTP-C-INIT_V5.tcl",
+        "../a1-gtp-proxy/src/GTP-C-clientAcceptV5.tcl",
+        "../a1-gtp-proxy/src/GTP-C-clientAcceptv3.tcl",
+        "../a1-gtp-proxy/src/GTP-C-clientAcceptv31.tcl",
+        "../a1-gtp-proxy/src/GTP-C-clientEgressV5.tcl",
+        "../a1-gtp-proxy/src/GTP-C-clientEgressv3.tcl",
+        "../a1-gtp-proxy/src/GTP-C-clientIngressV5.tcl",
+        "../a1-gtp-proxy/src/GTP-C-clientIngressv3.tcl",
+        "../a1-gtp-proxy/src/GTP-C-clientIngressv31.tcl",
+        "../a1-gtp-proxy/src/GTP-C-variables_V5.tcl",
+        "../a1-gtp-proxy/src/GTP-U_v4.tcl",
+        "../a1-gtp-proxy/src/GTP-Uv3.tcl",
+        "../a1-gtp-proxy/src/GTP-Uv31.tcl",
+        "../a1-gtp-proxy/src/lib_GTPutil_V4.tcl",
+        "../a1-gtp-proxy/src/lib_GTPutil_V5.tcl",
+        "../a1-gtp-proxy/src/lib_GTPutilv3.tcl",
+        "../a1-gtp-proxy/src/lib_LogUtil.tcl",
+    ] {
+        println!("formatting {path}");
+        let buf = std::fs::read(path)?;
+        let tokens = Lexer::new().lex(buf).expect("Failed to lex");
+        let ast = Parser::new().parse(&tokens).expect("Failed to parse");
+        let buf = Formatter::new().format(ast); // cursed interface
+        std::fs::write(path, buf)?;
+    }
     Ok(())
 }
